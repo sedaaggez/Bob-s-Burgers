46dfb6951893ba1d0d2bb564fdb0b22908d9af36
Seda <sedaaggez@aof.anadolu.edu.tr>
2022-03-30 20:14:31 +0300

    view binding

--- app/src/main/java/com/sedaaggez/bobsburgers/view/CharactersFragment.kt
+++ app/src/main/java/com/sedaaggez/bobsburgers/view/CharactersFragment.kt
@@ -1,7 +1,14 @@
 package com.sedaaggez.bobsburgers.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.sedaaggez.bobsburgers.R
 
-class CharactersFragment : Fragment() {
+class CharactersFragment : Fragment(R.layout.fragment_characters) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
