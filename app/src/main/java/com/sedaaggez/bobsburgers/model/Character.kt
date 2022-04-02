cf517a676e180af6b96eba12efa60b2801bc46f7
Seda <sedaaggez@aof.anadolu.edu.tr>
2022-04-02 18:33:54 +0300

    create model

---
+++ app/src/main/java/com/sedaaggez/bobsburgers/model/Character.kt
@@ -0,0 +1,15 @@
+package com.sedaaggez.bobsburgers.model
+
+data class Character(
+    val age: String,
+    val firstEpisode: String,
+    val gender: String,
+    val hairColor: String,
+    val id: Int,
+    val image: String,
+    val name: String,
+    val occupation: String,
+    val relatives: List<String>,
+    val url: String,
+    val voicedBy: String
+)
