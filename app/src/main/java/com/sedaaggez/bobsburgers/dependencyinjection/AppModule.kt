1f6de2f33dba8d79fc3b9117ba668d1fbe6a3e80
sedaaggez <sedaggez@gmail.com>
2022-04-06 08:26:18 +0300

    create BobsBurgersApplication and create AppModule add injectRetrofitAPI method

---
+++ app/src/main/java/com/sedaaggez/bobsburgers/dependencyinjection/AppModule.kt
@@ -0,0 +1,27 @@
+package com.sedaaggez.bobsburgers.dependencyinjection
+
+import com.sedaaggez.bobsburgers.api.RetrofitAPI
+import com.sedaaggez.bobsburgers.util.Util.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun injectRetrofitAPI() : RetrofitAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(RetrofitAPI::class.java)
+    }
+
+}
